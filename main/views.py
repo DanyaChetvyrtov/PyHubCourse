dcc7979a9e9b5b92e778f1351ed0fc3e19564afc
Semenov Danil <dabcom2013@gmail.com>
2024-03-30 13:09:50 +0300

    fixtures for goods were added

--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from goods.models import Categories
 
 # Create your views here.
 def index(request):
+    categories = Categories.objects.all()
+
     context = {
-        'title': 'Home - Главная',
+        'title': 'Home - главная',
         'content': 'Магазин мебели HOME',
+        'categories': categories
     }
+    
     return render(request, 'main/index.html', context)
 
 
