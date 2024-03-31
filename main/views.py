be5dd94bde8c6aac75378c621653c2525b0c29b2
Semenov Danil <dabcom2013@gmail.com>
2024-03-31 10:11:21 +0300

    Slug converter for product

--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-
     context = {
         'title': 'Home - главная',
         'content': 'Магазин мебели HOME',
