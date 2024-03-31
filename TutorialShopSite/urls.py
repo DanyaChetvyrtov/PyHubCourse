59fdf245577ec1b8c4ec74e061077ee9059308c6
Semenov Danil <dabcom2013@gmail.com>
2024-03-31 08:02:36 +0300

    Registered simple tag for categories

--- TutorialShopSite/urls.py
+++ TutorialShopSite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
-from TutorialShopSite.settings import DEBUG
+from TutorialShopSite import settings
 
 
 urlpatterns = [
@@ -10,7 +11,9 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog'))
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include('debug_toolbar.urls')),
         ]
+    
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
