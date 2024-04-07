bfd563ac6bf2cf42616fe1af392e2ec0e31071fe
Semenov Danil <dabcom2013@gmail.com>
2024-04-07 21:28:26 +0300

    All carts templates were added

--- TutorialShopSite/settings.py
+++ TutorialShopSite/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'main',
     'goods',
     'users',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -152,3 +153,4 @@ INTERNAL_IPS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/user/login/'
